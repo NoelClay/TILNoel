333b80a98b90096638c92012ca98554c79f443f3
NoelClay <asdf1578@naver.com>
2022-07-15 20:08:09 +0900

    d

---
+++ CodingTest/Study/week_10/Test_10.cpp
@@ -0,0 +1,38 @@
+//1번
+#if 0
+//균형잡힌 -> 올바른
+//길이는 1000
+//주어진 문제 재귀적으로 짜는 문제
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string p) {
+    string answer = "";
+
+    return answer;
+}
+#endif
+
+//2번
+#if 0
+//주어진 배열의 인덱스가 고유번호
+// 배열의 길이는 1만개
+// 장르배열과 플레이배열을 순차탐색하면서 장르의 종류와 각 장르별 가장 큰 수 2개를 저장 2만번
+// 
+// 연속된 큰수를 비교하니까 스택?
+//
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<string> genres, vector<int> plays) {
+    vector<int> answer;
+
+
+
+    return answer;
+}
+#endif
